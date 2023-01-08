cb22eef0845f4bf0c25cb8d2cc6e7de7c10e3813
abhar <abhar@isilis.fr>
2023-01-08 14:15:40 +0100

    define initial position and direction

--- src/test/java/com.mowitnow/MowerTest.java
+++ src/test/java/com.mowitnow/MowerTest.java
@@ -3,7 +3,7 @@ package com.mowitnow;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class MowerTest {
+class MowerTest {
 
     @Test
     void should_initiate_field_with_h5_and_l5() {
@@ -13,11 +13,28 @@ public class MowerTest {
 
 
         //When
-        mower = new Mower(line);
+        mower = new Mower(line, "0 0 L");
 
         //Then
-        Assertions.assertEquals(mower.getField().getLength(), 6);
-        Assertions.assertEquals(mower.getField().getHeight(), 6);
+        Assertions.assertEquals(6, mower.getField().getLength());
+        Assertions.assertEquals(6, mower.getField().getHeight());
+
+    }
+
+    @Test
+    void should_initiate_position_1_2_N_given_field_5_5() {
+        //Given
+        String line = "5 5";
+        String line2 = "1 2 N";
+        Mower mower;
+
+        //When
+        mower = new Mower(line, line2);
+
+        //Then
+        Assertions.assertEquals(1, mower.getPosition().getX());
+        Assertions.assertEquals(2, mower.getPosition().getY());
+        Assertions.assertEquals("N", mower.getDirection());
 
     }
 }
